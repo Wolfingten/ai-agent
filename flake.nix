c2bd8c32dd9d9e8bd75342740d55a7637368b270
wolfingten <jagu00005@stud.uni-saarland.de>
2026-01-02 18:14:25 +0100

    clean-up

--- flake.nix
+++ flake.nix
@@ -1,13 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = {
     self,
     nixpkgs,
-    nixpkgs-unstable,
     ...
   }: let
     systems = [
@@ -16,9 +14,6 @@
       "x86_64-darwin"
       "aarch64-darwin"
     ];
-    overlay-unstable = final: prev: {
-      unstable = nixpkgs-unstable.legacyPackages.${prev.system};
-    };
 
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
@@ -26,7 +21,6 @@
       system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [overlay-unstable];
         };
       in {
         default = pkgs.mkShell {
